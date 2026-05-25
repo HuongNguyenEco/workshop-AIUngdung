85ec51002bf82d9a25cf2c3c8a439fc21e27d3d7
HuongNguyenEco <huonglong0110.93@gmail.com>
2026-05-25 11:17:07 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
             fetch(GOOGLE_SCRIPT_URL, {
                 method: 'POST',
                 mode: 'no-cors',
-                body: formData
+                body: new URLSearchParams(formData)
             }).then(() => {
                 // Show success and QR
                 transferContent.textContent = transferMsg;
